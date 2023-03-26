dad238d4b98a7913c3c0baf4b7735ea7477e9ce1
emmaMarful <eamarful@gmail.com>
2023-03-26 16:18:25 -0700

    modified main

--- main.h
+++ main.h
@@ -1,27 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PRINT_F
+#define PRINT_F
 
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
-* struct convert - a structure for symbols eg. strings,
-* integers, chars etc.
-* @flg: The flag or operator
-* @f: The function
+* struct convert - flag
+*
+* @flg: The operator
+* @f: The function associated
 */
-
-typedef struct convert
+struct convert
 {
 	char *flg;
 	int (*f)(va_list);
-} con;
+};
+typedef struct convert con;
 
-int _putchar(char c);
-int pass(const char *format, con s_list[], va_list va);
-int _printf(const char *format, ...);
+/*Main functions*/
+int pass(const char *format, conver_t f_list[], va_list va);
+int _prntf(const char *format, ...);
 int _w_char(char);
 int p_char(va_list);
 int p_string(va_list);
@@ -30,15 +29,18 @@ int p_integer(va_list);
 int p_number(va_list);
 int p_binary(va_list);
 int p_reversed(va_list arg);
+int rot_13(va_list);
 int u_integer(va_list);
 int p_octal(va_list list);
 int p_hex(va_list list);
 int p_heX(va_list list);
-int rot_13(va_list);
+
+/*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int p_u_number(unsigned int);
+
 
 #endif
